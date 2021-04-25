247b802e639ccfd0e5299d61fd69ee388eb5c6fc
daryllast <daryl.last621@gmail.com>
2021-04-25 13:35:15 -0400

    user stories and about project

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 // API URL: http://api.openweathermap.org/data/2.5/weather?q={city name}&appid=3c9b9e5b66aff19a5f691fc73cf9210f
-// API Key: 3c9b9e5b66aff19a5f691fc73cf9210f
+// API Key for Weather Info: 3c9b9e5b66aff19a5f691fc73cf9210f
 
 let weather = {
     "apiKey": "3c9b9e5b66aff19a5f691fc73cf9210f",
+
     fetchWeather: function(city) {
         fetch("http://api.openweathermap.org/data/2.5/weather?q="
         + city 
@@ -22,11 +23,14 @@ let weather = {
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°F";
+        
+        let tempC = (Math.round(temp) - 32) * (5/9);
+        document.querySelector(".temp").innerText = Math.round(temp) + "°F / " + Math.round(tempC) + "°C";
+
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mi/h";
+        document.querySelector(".wind").innerText = "Wind Speed: " + Math.round(speed) + " mph";
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?city," + name + "')"
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
     
     search: function() {
@@ -44,3 +48,51 @@ document.querySelector(".search-bar").addEventListener("keyup", function(event){
     }
 
 });
+
+// Background Image API Key: 21328747-b6e2d72b9d549eeb510fa674d
+
+// let backgroundImages = {
+//     fetchBackgroundImage: function(cityName){
+//         {
+//             fetch("https://pixabay.com/api/?key=21328747-b6e2d72b9d549eeb510fa674d&q=
+//             +cityName&image_type=photo"
+    
+//             )
+//             .then((response) => response.json())
+//             .then((data) => this.displayImage(data));
+//         },
+    
+//         displayImage: function(data){
+//             const { cityName } = data;
+//             console.log(cityName);
+//             document.querySelector(".backgroundImage").src = "http://openweathermap.org/img/wn/" + cityName + "@2x.png";
+//             document.querySelector(".description").innerText = description;
+            
+//             let tempC = (Math.round(temp) - 32) * (5/9);
+//             document.querySelector(".temp").innerText = Math.round(temp) + "°F / " + Math.round(tempC) + "°C";
+    
+//             document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+//             document.querySelector(".wind").innerText = "Wind Speed: " + Math.round(speed) + " mph";
+//             document.querySelector(".weather").classList.remove("loading");
+//             document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+//         },
+        
+//         search: function() {
+//         this.fetchWeather(document.querySelector(".search-bar").value);
+//         },
+//     };
+    
+//     document.querySelector(".search button").addEventListener("click", function () {
+//     weather.search();
+//     });
+    
+//     document.querySelector(".search-bar").addEventListener("keyup", function(event){
+//         if (event.key == "Enter"){
+//             weather.search();
+//         }
+    
+//     });
+    
+//     }
+
+// }
