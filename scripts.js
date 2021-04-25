9502668742adc4961e9fea5eeb539051108086b4
daryllast <daryl.last621@gmail.com>
2021-04-24 21:54:31 -0400

    wireframes added

--- scripts.js
+++ scripts.js
@@ -24,7 +24,7 @@ let weather = {
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mi/h";
         document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?city," + name + "')"
     },
